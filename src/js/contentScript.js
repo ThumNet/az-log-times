5376675bca5e7690d37fc4223106dade552f8829
ThumNet <jeffreytummers@gmail.com>
2021-09-27 13:37:37 +0200

    feat: more visual (with timeline)

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -25,28 +25,85 @@ function readLog() {
   return entries;
 } 
 
-function createEntryElement(entry) {
+function createEntryElement(entry, index) {
   const div = document.createElement('div');
-  div.className = 'logentry'
-  div.innerHTML = `${entry.date.toJSON()} (${entry.diffInMs}ms) ${entry.text}`;
+  div.className = `logentry item${index}`;  
+  div.innerHTML = `${entry.date.toJSON()} ${entry.text}`;
+  const span = document.createElement('span');
+  span.className = 'duration';
+  span.innerText = `${entry.diffInMs}ms`
+  div.appendChild(span);
   return div;
 }
 
 function renderEntries(entries) {
-  let preElm = document.getElementsByTagName('pre')[0];
-  
-  entries.forEach(entry => {
-    document.body.appendChild(createEntryElement(entry));
+  const entriesElm = document.createElement('div');
+  entriesElm.className = 'entries';
+
+  entries.forEach((entry, ix) => {
+    entriesElm.appendChild(createEntryElement(entry, ix));
   });
+
+  return entriesElm;
+}
+
+function highlightEntry(ev) {
+  const highlightIx = ev.target.dataset.ix;
+  const toHighlight = document.querySelector(`.logentry.item${highlightIx}`);
+  toHighlight.classList.toggle('highlight');
+  if (toHighlight.classList.contains('highlight')){ 
+    toHighlight.scrollIntoView(false);
+  }
+}
+
+
+function renderTimeline(entries, totalMs) {
   
+  function map(value, valueRangeStart, valueRangeEnd, newRangeStart, newRangeEnd) {
+    return newRangeStart + (newRangeEnd - newRangeStart) * ((value - valueRangeStart) / (valueRangeEnd - valueRangeStart));
+  }
+
+  const container = document.createElement('div');
+  container.className = 'timeline';
+  entries.forEach((entry, ix) => {
+    const e = document.createElement('div');
+    let value = map(entry.diffInMs, 0, totalMs, 0, 100);
+    e.style.width = value + '%';
+    e.dataset.ix = ix;
+    e.addEventListener('mouseover', highlightEntry);
+    e.addEventListener('mouseout', highlightEntry);
+    container.appendChild(e);
+  });
+
+  return container;
+}
+
+function renderTotals(totalMs) {
+  const totalText = document.createElement('div');
+  totalText.className = 'total'
+  totalText.innerText = `Total duration: ${totalMs}ms`
+  return totalText;
+}
+
+function createDiv(className, child) {
+  const row = document.createElement('div');
+  row.className = className;
+  row.appendChild(child);
+  return row;
+}
+
+function render() {
+  const entries = readLog();
+  const totalMs = entries[entries.length-1].date - entries[0].date;
+
+  const box = document.createElement('div');
+  box.className = 'box';
+  box.appendChild(createDiv('row header', renderTimeline(entries, totalMs)));
+  box.appendChild(createDiv('row content', renderEntries(entries)));
+  box.appendChild(createDiv('row footer', renderTotals(totalMs)));
+  document.body.appendChild(box);  
+  const preElm = document.getElementsByTagName('pre')[0];  
   preElm.style.display = 'none';
 }
 
-function renderTimeLine(entries) {
-  console.log(entries.length);
-  console.log(entries[entries.length-1].date - entries[0].date);
-}
-
-let entries = readLog();
-renderTimeLine(entries);
-renderEntries(entries);
+render();
